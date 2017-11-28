eb7be7aacf6d0827be2071b6bde94eb819f20c7d
David Garcia <david.garcia.nete@gmail.com>
2017-11-28 15:44:28 -0500

    Initital commit

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,14 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Session',
+    'Zend\Mvc\Plugin\Prg',
+    'Zend\Mvc\Plugin\Identity',
+    'Zend\Mvc\Plugin\FlashMessenger',
+    'Zend\Mvc\Plugin\FilePrg',
+    'Zend\Form',
+    'Zend\Db',
+    'Zend\Cache',
     'Zend\Router',
     'Zend\Validator',
     'Application',
